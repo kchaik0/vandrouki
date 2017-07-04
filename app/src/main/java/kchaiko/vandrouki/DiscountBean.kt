a84f63d5fcb1fba6d303853d45a61cda4614d05e
kchaiko <chaiko.kostya@gmail.com>
2017-07-04 12:30:59 +0300

    First commit.

---
+++ app/src/main/java/kchaiko/vandrouki/DiscountBean.kt
@@ -0,0 +1,15 @@
+package kchaiko.vandrouki
+
+import java.util.*
+
+/**
+ * Bean for wrap discount
+ *
+ * Created by kchaiko on 03.07.2017.
+ */
+class DiscountBean {
+
+    var title: String? = null
+    var date: String? = null
+
+}
