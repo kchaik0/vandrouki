d232cc22330594d9c5198667b86dba4d29145bf0
kchaiko <chaiko.kostya@gmail.com>
2018-01-12 00:35:51 +0300

    Update with using Anko.

--- app/src/main/java/kchaiko/vandrouki/adapters/DiscountAdapter.kt
+++ app/src/main/java/kchaiko/vandrouki/adapters/DiscountAdapter.kt
@@ -1,15 +1,24 @@
 package kchaiko.vandrouki.adapters
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.support.annotation.AttrRes
+import android.support.annotation.ColorInt
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
+import android.text.TextUtils
+import android.util.TypedValue
+import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import kchaiko.vandrouki.R
 import kchaiko.vandrouki.VandroukiApp
 import kchaiko.vandrouki.beans.Discount
-import kotlinx.android.synthetic.main.item_discount.view.*
+import org.jetbrains.anko.*
+import org.jetbrains.anko.cardview.v7.cardView
+import kotlin.properties.Delegates
 
 /**
  * Adapter for showing discount items
@@ -19,32 +28,74 @@ import kotlinx.android.synthetic.main.item_discount.view.*
 class DiscountAdapter(private val dataset: List<Discount>, private val itemClick: (Discount) -> Unit)
     : RecyclerView.Adapter<DiscountAdapter.ViewHolder>() {
 
-    //private val dateFormat: SimpleDateFormat = SimpleDateFormat(DateFormats.SHOW_FORMAT.format, Locale.getDefault())
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val discountBean = dataset[position]
         holder.itemView.setOnClickListener {
             itemClick(discountBean)
         }
-        holder.tvTitle.text = discountBean.title
-        VandroukiApp.INSTANCE.picasso.load(discountBean.image).into(holder.ivType)
-        //holder.tvDate.text = dateFormat.format(discountBean.date)
+        holder.titleText.text = discountBean.title
+        VandroukiApp.INSTANCE.picasso.load(discountBean.image).into(holder.bgImage)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_discount, parent, false))
+        return DiscountItemUI().createView(AnkoContext.create(parent.context, parent)).tag as ViewHolder
     }
 
     override fun getItemCount(): Int {
         return dataset.size
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class DiscountItemUI : AnkoComponent<ViewGroup> {
 
-        val tvTitle: TextView = itemView.id_title
-        val ivType: ImageView = itemView.id_type
-        //val tvDate: TextView = itemView.id_date
+        @ColorInt
+        fun Context.getColorFromAttribute(@AttrRes attr: Int): Int {
+            val typedValue = TypedValue()
+            theme.resolveAttribute(attr, typedValue, true)
+            return typedValue.data
+        }
 
+        @SuppressLint("SetTextI18n")
+        override fun createView(ui: AnkoContext<ViewGroup>): View {
+            var titleText: TextView by Delegates.notNull()
+            var bgImage: ImageView by Delegates.notNull()
+            val itemView = with(ui) {
+                cardView {
+                    lparams(width = matchParent, height = wrapContent) {
+                        margin = dip(8)
+                    }
+                    cardElevation = px2dip(4)
+                    radius = px2dip(4)
+                    linearLayout {
+                        lparams(width = matchParent, height = wrapContent)
+                        orientation = LinearLayout.VERTICAL
+                        frameLayout {
+                            lparams(width = matchParent, height = matchParent)
+                            bgImage = imageView(R.drawable.test_bg) {
+                                scaleType = ImageView.ScaleType.CENTER_CROP
+                            }.lparams(width = matchParent, height = matchParent)
+                            titleText = themedTextView(theme = R.style.TextViewStyle_Light_Large,
+                                    text = "asdasdqweqwe qewbhjqhwej qbwjebqwebhj qbwhjebqhwehj bqhwebhjqjbhwe bhqwjbehqjb bhjqwebhjqwbe hbqhwjebhjq bhjqbwehjbqjwe") {
+                                ellipsize = TextUtils.TruncateAt.END
+                                maxLines = 1
+                                padding = dip(8)
+                                backgroundColor = context.getColorFromAttribute(R.attr.colorCardNameBg)
+                            }.lparams(width = matchParent, height = wrapContent, gravity = Gravity.BOTTOM)
+                        }
+                        themedTextView(theme = R.style.TextViewStyle_Secondary_Normal,
+                                text = "asdqweqweqweqweqweqwe qwdasd qweqwe asdqdqweqwe qweqweqwe") {
+                            ellipsize = TextUtils.TruncateAt.END
+                            maxLines = 3
+                        }.lparams(width = wrapContent, height = wrapContent) {
+                            margin = dip(8)
+                        }
+                    }
+                }
+            }
+            itemView.tag = ViewHolder(itemView, titleText, bgImage)
+            return itemView
+        }
     }
+
+    class ViewHolder(itemView: View, val titleText: TextView, val bgImage: ImageView) : RecyclerView.ViewHolder(itemView)
 
 }
