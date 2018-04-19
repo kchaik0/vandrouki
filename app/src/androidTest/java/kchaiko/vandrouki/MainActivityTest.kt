34a93ccc9aca32260b7963b67cfa1d17b4a9deff
Kanstantsin.Chaiko <Kanstantsin.Chaiko@itechart-group.com>
2018-04-19 17:10:02 +0300

    Add some UI test using Espresso framework.

---
+++ app/src/androidTest/java/kchaiko/vandrouki/MainActivityTest.kt
@@ -0,0 +1,27 @@
+package kchaiko.vandrouki
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.doesNotExist
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import kchaiko.vandrouki.activity.MainActivity
+import org.hamcrest.Matchers.not
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    val activityRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+    @Test
+    fun generalUI() {
+        onView(withId(R.id.am_recycler)).check(matches(not(doesNotExist())))
+    }
+
+}
---
+++ app/src/androidTest/java/kchaiko/vandrouki/SplashActivityTest.kt
@@ -0,0 +1,24 @@
+package kchaiko.vandrouki
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import junit.framework.Assert.assertEquals
+import kchaiko.vandrouki.activity.SplashActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SplashActivityTest {
+
+    @Rule
+    @JvmField
+    val activityRule = ActivityTestRule<SplashActivity>(SplashActivity::class.java)
+
+    @Test
+    fun generalUI() {
+        assertEquals("kchaiko.vandrouki", InstrumentationRegistry.getTargetContext().packageName)
+    }
+
+}
