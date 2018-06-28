7a68302f67538509b28d8f325e77584e6e4d5ff3
Kanstantsin.Chaiko <Kanstantsin.Chaiko@itechart-group.com>
2018-06-28 16:13:33 +0300

    Update project for bitrise

--- app/src/main/kotlin/kchaiko/vandrouki/activity/MainActivity.kt
+++ app/src/main/kotlin/kchaiko/vandrouki/activity/MainActivity.kt
@@ -33,5 +33,7 @@ class MainActivity : BaseActivity() {
         viewModel.dataDelegate { binding.adapter?.addItems(it) }
                 .errorDelegate { proceedError(it) }
                 .provideData()
+
+
     }
 }
